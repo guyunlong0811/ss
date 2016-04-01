b95c812b9bf155e6d47e6ae61eacfc9d607bdead
guyunlong <guyunlong0811@126.com>
2016-04-01 15:44:29 +0800

    snda merge commit

---
+++ script/Common/Conf/snda.php
@@ -0,0 +1,14 @@
+<?php
+//static
+define('DB_STATIC_TYPE', 'mysql');
+define('DB_STATIC_HOST', '127.0.0.1');
+define('DB_STATIC_USER', 'root');
+define('DB_STATIC_PWD', 'Lsen1M?7s');
+define('DB_STATIC_PORT', '3306');
+define('DB_STATIC_NAME', 'zettaiStatic');
+define('DB_STATIC_CHARSET', 'utf8');
+
+//URL
+define('UC_URL', 'http://123.59.98.22:24602/user.php');
+define('OPE_URL', 'http://123.59.98.22:24603/admin.php');
+define('SCRIPT_URL', 'http://123.59.98.22:24604/index.php');
